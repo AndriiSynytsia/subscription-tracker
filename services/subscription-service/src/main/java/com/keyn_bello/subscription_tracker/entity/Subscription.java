079119ab2976c3410a9aeb06b0468bc5fd302dda
Andrii Synytsia <asynytsia.work@gmail.com>
2025-09-03 20:56:41 -0600

    fix: delete unnecessary import

--- services/subscription-service/src/main/java/com/keyn_bello/subscription_tracker/entity/Subscription.java
+++ services/subscription-service/src/main/java/com/keyn_bello/subscription_tracker/entity/Subscription.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
