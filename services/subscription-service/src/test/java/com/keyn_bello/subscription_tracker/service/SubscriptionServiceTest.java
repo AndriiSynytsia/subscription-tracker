34f3c3ff74fc7f0ea91d82b59e3ac9771001f616
Andrii Synytsia <asynytsia.work@gmail.com>
2025-09-04 21:18:36 -0600

    Added changes according to SonarCube and CodeRabbit

--- services/subscription-service/src/test/java/com/keyn_bello/subscription_tracker/service/SubscriptionServiceTest.java
+++ services/subscription-service/src/test/java/com/keyn_bello/subscription_tracker/service/SubscriptionServiceTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 class SubscriptionServiceTest {
 
     @Test
-        /*
-         * This is an empty test method that verifies if the Spring application context loads successfully.
-         */
     void contextLoads() {
+        // loads the Spring context; test fails if context cannot start
     }
 }
