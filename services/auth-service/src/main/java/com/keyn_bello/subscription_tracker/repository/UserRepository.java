cd6a3d067719c29d46bbe18343d31af6e656eaf7
Andrii Synytsia <asynytsia.work@gmail.com>
2025-09-10 21:30:43 -0600

    Implemented UserRepository with basic implementation

--- services/auth-service/src/main/java/com/keyn_bello/subscription_tracker/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/keyn_bello/subscription_tracker/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.keyn_bello.subscription_tracker.repository;
 
-public class UserRepository {
+import com.keyn_bello.subscription_tracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
