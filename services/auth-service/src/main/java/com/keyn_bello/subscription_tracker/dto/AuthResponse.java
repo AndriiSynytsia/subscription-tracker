079cee19ce9108cbe2fc1ef5c4e2826ad3ea9b02
Andrii Synytsia <asynytsia.work@gmail.com>
2025-09-14 14:00:41 -0600

    feat: Implemented AuthResponse with user information

--- services/auth-service/src/main/java/com/keyn_bello/subscription_tracker/dto/AuthResponse.java
+++ services/auth-service/src/main/java/com/keyn_bello/subscription_tracker/dto/AuthResponse.java
@@ -1,4 +1,35 @@
 package com.keyn_bello.subscription_tracker.dto;
 
-public class AuthResponse {
+import com.keyn_bello.subscription_tracker.entity.User;
+import jakarta.validation.constraints.NotNull;
+
+public record AuthResponse(
+        @NotNull
+        String token,
+        @NotNull
+        UserInfo user
+) {
+    public static AuthResponse from(String token, User user) {
+        UserInfo userInfo = new UserInfo(
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getId(),
+                user.getProfileImageUrl()
+        );
+        return new AuthResponse(token, userInfo);
+    }
+
+    public record UserInfo(
+            @NotNull
+            String email,
+            @NotNull
+            String firstName,
+            @NotNull
+            String lastName,
+            @NotNull
+            Long userId,
+            String profileImageUrl
+    ) {
+    }
 }
