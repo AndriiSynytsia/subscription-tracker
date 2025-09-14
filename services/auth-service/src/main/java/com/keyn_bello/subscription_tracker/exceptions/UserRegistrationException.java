9c6a3b2be22aea2bbaa009d725d063642f7c5584
Andrii Synytsia <asynytsia.work@gmail.com>
2025-09-14 17:26:06 -0600

    feat: Improved exception handling, JWT authentication. Added dependencies for API documentation

---
+++ services/auth-service/src/main/java/com/keyn_bello/subscription_tracker/exceptions/UserRegistrationException.java
@@ -0,0 +1,7 @@
+package com.keyn_bello.subscription_tracker.exceptions;
+
+public class UserRegistrationException extends RuntimeException {
+    public UserRegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
