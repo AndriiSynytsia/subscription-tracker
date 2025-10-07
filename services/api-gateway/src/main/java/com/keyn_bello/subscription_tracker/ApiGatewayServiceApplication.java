ea510ff71e287c3609b709e7ac6566c0352a5f63
Andrii Synytsia <asynytsia.work@gmail.com>
2025-10-06 20:38:57 -0600

    feat: removed exclusions from application class

--- services/api-gateway/src/main/java/com/keyn_bello/subscription_tracker/ApiGatewayServiceApplication.java
+++ services/api-gateway/src/main/java/com/keyn_bello/subscription_tracker/ApiGatewayServiceApplication.java
@@ -2,18 +2,11 @@ package com.keyn_bello.subscription_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        FlywayAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        DataSourceAutoConfiguration.class
-})public class ApiGatewayServiceApplication {
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
 }
