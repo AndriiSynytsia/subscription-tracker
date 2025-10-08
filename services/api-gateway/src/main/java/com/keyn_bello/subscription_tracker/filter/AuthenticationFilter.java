5c758753b531fabaf2cd969637e3aa636cba21e8
Andrii Synytsia <asynytsia.work@gmail.com>
2025-10-07 19:50:54 -0600

    feat: implemented filter to verify that routes have valid headers and validate the JWT token

---
+++ services/api-gateway/src/main/java/com/keyn_bello/subscription_tracker/filter/AuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.keyn_bello.subscription_tracker.filter;
+
+import com.keyn_bello.subscription_tracker.uitl.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationFilter(JwtUtil jwtUtil) {
+        super(Config.class);
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || authHeader.startsWith("Bearer ")) {
+                return onError(exchange);
+            }
+
+            String token = authHeader.substring(7);
+
+            if (!jwtUtil.isTokenValid(token)) {
+                return onError(exchange);
+            }
+
+            String username = jwtUtil.extractUsername(token);
+            exchange.getRequest().mutate()
+                    .header("X-User-Id", username)
+                    .build();
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {
+        // Define configuration properties here if needed
+    }
+}
