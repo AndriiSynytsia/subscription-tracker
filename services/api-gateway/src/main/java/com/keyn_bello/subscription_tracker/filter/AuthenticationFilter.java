278cbd2dce179b299ebfe9e3e64fd86348af1605
Andrii Synytsia <asynytsia.work@gmail.com>
2025-10-08 20:04:47 -0600

    fix: delete unnecessary import

--- services/api-gateway/src/main/java/com/keyn_bello/subscription_tracker/filter/AuthenticationFilter.java
+++ services/api-gateway/src/main/java/com/keyn_bello/subscription_tracker/filter/AuthenticationFilter.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
